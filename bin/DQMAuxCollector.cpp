9760406b600bf94aa0993dd95b370c888d46b2b0
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2008-01-11 15:47:51 +0000

    DQMServices internal cleanup, first of the major migrations planned in 2007. Move code from {Core,Node}ROOT, UI, QualityTests and Daemon into Core. Leave forwarding headers and build files into the old packages so subsystem DQM code continues to build unchanged.

---
+++ bin/DQMAuxCollector.cpp
@@ -0,0 +1,62 @@
+#include <string>
+#include <iostream>
+#include "TApplication.h"
+#include "TROOT.h"
+
+#include "DQMServices/Core/interface/AuxCollectorRoot.h"
+
+using std::string;
+
+class DummyAuxCollector : public AuxCollectorRoot
+{
+
+ public:
+  virtual void process(){}
+  DummyAuxCollector(string host, int port_no) : 
+    AuxCollectorRoot(host, "AuxCollector", port_no)
+  {
+    inputAvail_=true;
+    run();
+  }
+  static 
+  AuxCollectorRoot *instance(string host, 
+			     int port_no = AuxCollectorRoot::defListenPort)
+  {
+    if(instance_==0)
+      instance_ = new DummyAuxCollector(host, port_no);
+    return instance_;
+  }
+
+ private:
+  static AuxCollectorRoot * instance_;
+};
+
+AuxCollectorRoot *DummyAuxCollector::instance_=0; 
+
+extern void InitGui(); 
+VoidFuncPtr_t initfuncs[] = { InitGui, 0 };
+TROOT producer("producer","Simple histogram producer",initfuncs);
+
+// usage: DQMAuxCollector <collector hostname> <port_no>
+int main(int argc, char *argv[])
+{
+  string hostname;
+  if(argc >= 2)
+    hostname = argv[1];
+
+  // default port #
+  int port_no = 9090;
+  if(argc >= 3) port_no = atoi(argv[2]);
+
+  try
+    {
+      DummyAuxCollector::instance(hostname, port_no); 
+    }
+  catch (...)
+    {
+      return -1;
+    }
+  //  gROOT->SetBatch(kTRUE);
+  //TApplication app("app",&argc,argv);
+  return 0;
+}
